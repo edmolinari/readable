1dd32d9696361e8a5b20d7e27e8ff3908e219142
Eduardo Molinari <emolinari@groupon.com>
2018-06-06 07:17:10 -0700

    material ui components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { List, ListItem, withStyles } from "material-ui";
+
+import footerStyle from "assets/jss/material-dashboard-react/footerStyle";
+
+function Footer({ ...props }) {
+  const { classes } = props;
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+            <ListItem className={classes.inlineBlock}>
+              <a href="#categories" className={classes.block}>
+                Categories
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a href="#Posts" className={classes.block}>
+                Posts
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a href="#about" className={classes.block}>
+                About
+              </a>
+            </ListItem>
+          </List>
+        </div>
+        <p className={classes.right}>
+          <span>
+            &copy; {1900 + new Date().getYear()}{" "}
+            <a href="http://github.com/edmolinari/" className={classes.a}>
+              Ed Molinari's
+            </a> Redux + Material UI project
+          </span>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(footerStyle)(Footer);
